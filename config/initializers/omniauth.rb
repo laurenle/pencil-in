cc44ee46eef9632eb7f02252b29364d2e9c6280a
Lauren <laleung94@gmail.com>
2016-04-24 13:15:44 -0400

    troubleshooting oauth

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+#config/initalizers/omniauth.rb
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
+  scope: ['email',
+    'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar'],
+    access_type: 'offline'}
+end
