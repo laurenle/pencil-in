792d69707f997ed111e054102bde2001deaa0f30
Lauren <laleung94@gmail.com>
2016-04-24 14:37:16 -0400

    trying new request token frm google code..

--- app/models/token.rb
+++ app/models/token.rb
@@ -13,16 +13,21 @@ class Token < ActiveRecord::Base
   end
 
   def request_token_from_google
-    url = URI("https://accounts.google.com/o/oauth2/token")
-    Net::HTTP.post_form(url, self.to_params)
+    uri = URI.parse("https://www.googleapis.com/oauth2/v3/token")
+    http = Net::HTTP.new(uri.host, uri.port)
+    http.use_ssl = true
+    http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    request = Net::HTTP::Post.new(uri.request_uri)
+    request.set_form_data(self.to_params)
+    http.request(request)
   end
 
   def refresh!
     response = request_token_from_google
     data = JSON.parse(response.body)
     update_attributes(
-    access_token: data['access_token'],
-    expires_at: Time.now + (data['expires_in'].to_i).seconds)
+      access_token: data['access_token'],
+      expires_at: Time.now + (data['expires_in'].to_i).seconds)
   end
 
   def expired?
