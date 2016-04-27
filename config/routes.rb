7b0f2bc82015b27bc88322b1d1f267f2c2782f07
Lauren <laleung94@gmail.com>
2016-04-27 18:09:37 -0400

    generate vanilla controller for list

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  get 'list/new'
+
+  get 'list/order'
+
 end
 
 # config/routes.rb
 PencilIn::Application.routes.draw do
+  get 'list/new'
+
+  get 'list/order'
+
   root to: 'welcome#index'
   resources :sessions
   get "/auth/:provider/callback" => 'sessions#create'
