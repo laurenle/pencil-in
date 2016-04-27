4e9198922bdacda712b14e05e3f0ef90de1dd3ee
Lauren <laleung94@gmail.com>
2016-04-27 17:10:31 -0400

    stable api calls, but with csrf errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ end
 # config/routes.rb
 PencilIn::Application.routes.draw do
   root to: 'welcome#index'
-  resources :sessions, only: :index
+  resources :sessions
   get "/auth/:provider/callback" => 'sessions#create'
 end
