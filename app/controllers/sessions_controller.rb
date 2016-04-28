f822e473fba48f49756af50add907fb93791f131
Lauren <laleung94@gmail.com>
2016-04-28 03:46:13 -0400

    Added login error

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:email])
     if @user.nil? || @user.password != params[:password]
+      flash[:error] = "No matching email/password found."
       render :new
     else
       session[:user_id] = @user.id
