20b32d15ac016949e5ec16f3061f1457bb8a805e
Lauren <laleung94@gmail.com>
2016-04-28 02:21:22 -0400

    obvious bug is obvious

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(email: params[:name])
+    @user = User.find_by(email: params[:email])
     if @user.nil? || @user.password != params[:password]
       render :new
     else
