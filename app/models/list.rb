82bc192895e75a92b5b1f473840b34e1e43ba4f6
Lauren <laleung94@gmail.com>
2016-04-28 13:24:54 -0400

    removed scaffolds for list, listitem

--- app/models/list.rb
+++
@@ -1,5 +0,0 @@
-class List < ActiveRecord::Base
-  validates :user_id, presence: true
-  has_many :list_items, dependent: :destroy
-  belongs_to :user
-end
--- app/models/list_item.rb
+++
@@ -1,5 +0,0 @@
-class ListItem < ActiveRecord::Base
-  validates :description, :list_id, 
-  :priority, :start_time, :end_time, presence: true
-  belongs_to :list
-end
