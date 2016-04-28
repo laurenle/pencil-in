381e0498470cd5ec77fcbafeb4c69b66ef5cfcad
Lauren <laleung94@gmail.com>
2016-04-28 03:43:47 -0400

    Added code for getting calendar id, which is incidentally just your email address on google

--- app/controllers/authorize_controller.rb
+++ app/controllers/authorize_controller.rb
@@ -8,8 +8,16 @@ class AuthorizeController < ApplicationController
     client.authorization.access_token = @token
     service = client.discovered_api('calendar', 'v3')
     @result = client.execute(
-      :api_method => service.calendar_list.list,
-      :parameters => {},
+      :api_method => service.freebusy.query,
+      :body => JSON.dump({
+          "timeMin": DateTime.now.to_s,
+          "timeMax": (DateTime.now + 7.days).to_s,
+          "items": [
+            {
+              "id": @auth["info"]["email"]
+            }
+          ]
+        }),
       :headers => {'Content-Type' => 'application/json'})
   end
 
