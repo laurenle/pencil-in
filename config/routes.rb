00ad2e2e71b9606d4745d2ec4b7e017bc82b3231
Lauren <laleung94@gmail.com>
2016-04-28 00:43:59 -0400

    Jank login and logout with nav working

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ PencilIn::Application.routes.draw do
   root to: 'welcome#index'
   resources :sessions
   get "/auth/:provider/callback" => 'sessions#create'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
 end
