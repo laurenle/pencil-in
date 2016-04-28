b811f786beef5aa6a3f688379a6226a65d052672
Lauren <laleung94@gmail.com>
2016-04-28 02:40:22 -0400

    Added signup validators

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,9 @@ class User < ActiveRecord::Base
 
   validates :name, :email, :password_hash, presence: true
   validates :name, length: { minimum: 2 }
+  validates :password, length: { minimum: 6 }
   validates :email, uniqueness: true
+  validate :valid_email?, :capitalized?
   has_many :lists, dependent: :destroy
 
   def password
@@ -14,4 +16,17 @@ class User < ActiveRecord::Base
     @password = Password.create(new_password)
     self.password_hash = @password
   end
+
+  def valid_email?
+    unless @email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+      errors.add(:base, 'Please enter a valid email address.')
+    end
+  end
+
+  def capitalized?
+    unless name.nil? || name.empty?
+      matches = name.strip[0].match(/[A-Z]/)
+      errors.add(:base, 'Name must be capitalized.') if matches.to_s.empty?
+    end
+  end  
 end
