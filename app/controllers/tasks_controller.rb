2e1e9c833c12583ed77d93821b80a3f57cafa7cf
Lauren <laleung94@gmail.com>
2016-04-29 02:03:57 -0400

    rubocop controllers

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -24,7 +24,8 @@ class TasksController < ApplicationController
   # POST /tasks
   def create
     @temp = Task.new(task_params)
-    @temp.start_time = next_avail(@temp.duration, params[:calendar_id])
+    @calendar = Calendar.find(params[:calendar_id])
+    @temp.start_time = next_avail(@temp.duration, @calendar)
 
     if @temp.save
       redirect_to @temp, notice: 'Task was successfully created.'
@@ -62,13 +63,17 @@ class TasksController < ApplicationController
 
   private
 
-  def next_avail(mins, id)
-    cal = Calendar.find(id)
-    for i in 1..(cal.events.size - 1)
-      diff = cal.events[i].end - cal.events[i - 1].start
-      return cal.events[i].end unless (diff * 24 * 60).to_i < mins
+  def next_avail(mins, cal)
+    cal.events.each_with_index do |event, i|
+      break unless i < cal.events.size
+      return cal.events[i].end unless time_difference_minutes(
+        cal.events[i].end, event.start) < mins
     end
     nil
+  end
+
+  def time_difference_minutes(time1, time2)
+    ((time1 - time2) * 24 * 60).to_i
   end
 
   # Use callbacks to share common setup or constraints between actions.
