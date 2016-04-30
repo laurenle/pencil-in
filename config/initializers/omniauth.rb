d11cf31015a8c60273a7413062a6046a24c56504
Lauren <laleung94@gmail.com>
2016-04-30 13:55:30 -0400

    changed heroku redirect uri

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,6 +3,6 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV['CLIENT_ID'], ENV['CLIENT_SECRET'], {
   scope: 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar',
   access_type: 'offline',
-  redirect_uri:'http://cbf8bf07.ngrok.io/auth/google_oauth2/callback'
+  redirect_uri:'https://pencil-in-app.herokuapp.com/auth/google_oauth2/callback'
   }
 end
